15bdcde104cfb4992486a2d9296186a8ada253ca
Confeder <heknypenky1337@gmail.com>
2024-06-19 11:03:02 +0200

    Serialize team members/users (read only)

--- photoroom/serializers/team.py
+++ photoroom/serializers/team.py
@@ -1,8 +1,17 @@
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email"]
 
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ["id", "name"]
+        fields = ["id", "name", "users"]
+        depth = 2
+
+    users = UserSerializer(many=True, read_only=True, source="user_set")
